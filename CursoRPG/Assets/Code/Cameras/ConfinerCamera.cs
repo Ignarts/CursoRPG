d84c2e4c8a476c41ed2fd3cf91da526cfdca95c0
Ignacio Melendez <ignaciomelendez2013@gmail.com>
2024-04-17 16:39:43 +0200

    CinemachineConfiners

--- CursoRPG/Assets/Code/Cameras/ConfinerCamera.cs
+++ CursoRPG/Assets/Code/Cameras/ConfinerCamera.cs
@@ -1,7 +1,35 @@
-using Cinemachine;
 using UnityEngine;
 
 public class ConfinerCamera : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+    #region Private Attributes
+
+    [SerializeField] private GameObject _virtualCamera;
+
+    #endregion
+
+    #region MonoBehaviour Methods
+
+    private void Awake()
+    {
+        _virtualCamera.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _virtualCamera.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _virtualCamera.SetActive(false);
+        }
+    }
+
+    #endregion
 }
